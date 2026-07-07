35e8d19b14b24c8110559794e88623343e02be83
Napat Kulnarong <napat.k@NapatKs-Macbook-M1-Pro.local>
2026-07-07 16:27:10 +0700

    chore: fix bugs

--- code.ts
+++ code.ts
@@ -1,7 +1,7 @@
 figma.showUI(__html__, { width: 450, height: 550 });
 
 // SECURITY DATA LINK CONFIGURATION
-const GITHUB_TOKEN = "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"; 
+const GITHUB_TOKEN = "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"; 
 const REPO_OWNER = "NapatKulnarong";
 const REPO_NAME = "DesignTokenSync";
 const FILE_PATH = "DESIGN.md";
